6c784d7caddffe0fc1bd9836625c56678bbe7180
ustubyo <yuan123173@gmail.com>
2020-11-09 11:59:05 +0800

    Initial commit

---
+++ Assets/Charactercontoller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Charactercontoller : MonoBehaviour
+{
+    //Movement/rotation speed 
+    public float speed;
+    public float rotatespeed;
+    public GameObject scoreText;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, 0, Time.deltaTime * speed);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position += new Vector3(0, 0, Time.deltaTime * speed);
+            transform.position += transform.forward * -speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotatespeed, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * -rotatespeed, 0));
+        }
+
+        scoreText.GetComponent<Text>().text = "Position" + transform.position; 
+    }
+}
